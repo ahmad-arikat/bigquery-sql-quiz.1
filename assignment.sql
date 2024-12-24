85072c70705d3fd06be2cf8fa51058d13a1b7de7
ahmad-arikat <ahmadarikat03@gmail.com>
2024-12-24 22:59:55 +0300

    Add files via upload

---
+++ assignment.sql
@@ -0,0 +1,12 @@
+﻿SELECT
+ refresh_date AS Day,
+ term AS Top_Term,
+ rank
+FROM `bigquery-public-data.google_trends.top_terms`
+WHERE
+ rank BETWEEN 1 AND 3
+ -- Choose only the top term each day.
+ AND refresh_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 4 WEEK)
+ -- Filter to the last 2 weeks.
+GROUP BY week, Top_Term, rank
+ORDER BY Day DESC
